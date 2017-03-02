21a69b2b524a72684cbe0b8279f069d7cea91f95
Clarefollett <chlaw101@gmail.com>
2017-03-02 10:34:32 -0800

    created table

--- src/index.js
+++ src/index.js
@@ -7,6 +7,22 @@ import './index.css';
 //   document.getElementById('root')
 // );
 
-fetch('http://www.omdbapi.com/?s=Back+To+The+Future&plot=short&r=json')
-  .then(res => res.json())
-  .then(movies => )
+// fetch('http://www.omdbapi.com/?s=Back+To+The+Future&plot=short&r=json')
+//   .then(res => res.json())
+//   .then(movies => )
+
+function App(props) {
+  return (
+    <table>
+      <tbody>
+        <tr>
+          <th>Title</th>
+          <th>Year</th>
+          <th>imdbID</th>
+          <th>Type</th>
+          <th>Poster</th>
+        </tr>
+      </tbody>
+    </table>
+  )
+}
