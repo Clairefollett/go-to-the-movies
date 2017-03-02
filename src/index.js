11d5f65ff0f6596a61c46706f5fa69db40396aa1
Clarefollett <chlaw101@gmail.com>
2017-03-02 10:39:45 -0800

    figuring it out really slowly but learning a lot

--- src/index.js
+++ src/index.js
@@ -2,16 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-// ReactDOM.render(
-//   <App />,
-//   document.getElementById('root')
-// );
 
-// fetch('http://www.omdbapi.com/?s=Back+To+The+Future&plot=short&r=json')
-//   .then(res => res.json())
-//   .then(movies => )
+
+fetch('http://www.omdbapi.com/?s=Back+To+The+Future&plot=short&r=json')
+  .then(res => res.json())
+  .then(movies => 
+    ReactDOM.render(
+      <App />,
+      document.getElementById('root')
+  )
+)
 
 function App(props) {
+  const allMovies = props.movies.Search.map(movies)
   return (
     <table>
       <tbody>
