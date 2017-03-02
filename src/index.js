e6f16f7f58741ef9d46bbcc8dd540a876a391978
Clarefollett <chlaw101@gmail.com>
2017-03-02 11:57:28 -0800

    got it working, just playing around wtih css

--- src/index.js
+++ src/index.js
@@ -3,48 +3,50 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 
+ReactDOM.render(
+  <App movies={[]} loading={true} />,
+  document.getElementById('root')
+)
 
-fetch('http://www.omdbapi.com/?s=Back+To+The+Future&plot=short&r=json')
+fetch('http://www.omdbapi.com/?s=Back+To+The+Future&plot=short&r=json&type=movie')
   .then(res => res.json())
   .then(movies => 
     ReactDOM.render(
-      <App movies={movies} />,
+      <App movies={movies} loading={false} />,
       document.getElementById('root')
   )
 )
 
 function App(props) {
+  if (props.loading) return <p>Loading movies!</p>;
   const allMovies=props.movies.Search.map(movie => (
-      <List
-        movie={movie}
-        key={movie.imdbID}
-      />
+        <List
+          movie={movie}
+          key={movie.imdbID}
+        />
     )
   )
-  return (
-    <table>
-      <tbody>
-        <tr>
-          <th>Title</th>
-          <th>Year</th>
-          <th>imdbID</th>
-          <th>Type</th>
-          <th>Poster</th>
-        </tr>
-        {allMovies}
-      </tbody>
-    </table>
-  )
-}
+
+    return (
+      <table className='no-spacing'>
+        <tbody>
+          <tr>
+            <th className='poster'>Poster</th>
+            <th className='title'>Title</th>
+            <th className='year'>Year</th>
+          </tr>
+          {allMovies}
+        </tbody>
+      </table>
+    )
+  }
 
 function List(props) {
   return (
     <tr>
-      <td>{props.movie.Title}</td>
-      <td>{props.movie.Year}</td>
-      <td>{props.movie.imdbID}</td>
-      <td>{props.movie.Type}</td>
       <td><img alt='moviePoster' src={props.movie.Poster} /></td>
+      <td className='titleYo'>{props.movie.Title}</td>
+      <td>{props.movie.Year}</td>
     </tr>
   )
 }
