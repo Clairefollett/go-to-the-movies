313e6bcf88589cf165a777dcb5f8796449df2a65
Clarefollett <chlaw101@gmail.com>
2017-03-02 10:49:59 -0800

    it's not rendering for some reason

--- src/index.js
+++ src/index.js
@@ -14,7 +14,13 @@ fetch('http://www.omdbapi.com/?s=Back+To+The+Future&plot=short&r=json')
 )
 
 function App(props) {
-  const allMovies = props.movies.Search.map(movies)
+  const allMovies=props.movies.Search.map(movie => (
+      <List
+        movie={movie}
+        key={movie.imdbID}
+      />
+    )
+  )
   return (
     <table>
       <tbody>
@@ -25,7 +31,20 @@ function App(props) {
           <th>Type</th>
           <th>Poster</th>
         </tr>
+        {allMovies}
       </tbody>
     </table>
+  )
+}
+
+function List(props) {
+  return (
+    <tr>
+      <td>{props.movie.Title}</td>
+      <td>{props.movie.Year}</td>
+      <td>{props.movie.imdbID}</td>
+      <td>{props.movie.Type}</td>
+      <td><img alt='moviePoster' src={props.movie.Poster} /></td>
+    </tr>
   )
 }
